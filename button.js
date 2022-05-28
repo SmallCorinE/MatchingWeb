866cfa8311e4ad1439266bc76d21d3b67383eca8
SmallCorinE <qqqqmmss11@gmail.com>
2022-05-29 05:03:30 +0900

    대충 뼈대

---
+++ button.js
@@ -0,0 +1,38 @@
+var Link = {
+	setLinkColor:function (color){
+		// var alist = document.querySelectorAll('a');
+		// var i = 0;
+		// while(i<alist.length){
+		// 	console.log(alist[i]);
+		// 	alist[i].style.color=color;
+		// 	i=i+1;
+		// }
+		$('a').css('color', color);
+	}
+}
+var Body = {
+	setLetterColor:function (color){
+		// document.querySelector('body').style.color=color;
+		$('body').css('color', color);
+	},
+	setBackGColor:function (color){
+		// document.querySelector('body').style.backgroundColor=color;
+		$('body').css('backgroundColor', color);
+	}
+}
+
+	function contrastHandler(self){
+		var target = document.querySelector('body');
+		if(self.value === 'Make it bright'){
+			Body.setBackGColor('white')
+			Body.setLetterColor('black')
+			self.value = 'Make it dark';
+			Link.setLinkColor('pink')
+
+		} else {
+			Body.setBackGColor('black')
+			Body.setLetterColor('white')
+			self.value = 'Make it bright';
+			Link.setLinkColor('pink');
+		}
+	}
